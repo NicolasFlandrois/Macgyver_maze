e41f424565a386b304baa38f68eea02a0efcc976
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-01-15 17:02:18 +0100

    continue coding according to my pointers. Display maze output. and started the move function in a if/elif/else loop.

--- mazeruner.py
+++ mazeruner.py
@@ -1,6 +1,6 @@
 #! bin/ python3
 from math import sqrt as sqrt
-
+import re
 #name = input("What is your Name? ")
 #This variable will be used to timestamp and keep traces of various plays and duration to solve the maze. Like a Score records.
 #format of this data base : Timestamp, Name, Maze#, DurationDelta(Timestamp @ Finish - Timestamp @ start)
@@ -10,31 +10,52 @@ with open("maze1.txt") as f:
 	maze = [int(n) for n in f.read().replace(" ", "").replace("\n", "")]
 	print(maze)
 
-row_len = sqrt(len(maze))
-#print(row_len) #test run command fro row length. Checked OK, it works.
+row_len = sqrt(len(maze))+1
+print("row length: ", row_len) #test run command fro row length. Checked OK, it works.
 
-def maze_view():
+def maze_view(maze):
 #retranscrire la liste sous forme de tableau type maze1.txt
 #changer la liste en string = Ok, Done
-	maze_view = "".join([str(i) for i in maze])
+	global row_len
+	maze_view = []
+	for pos in enumerate(maze):
+		if ((float(pos[0]) + row_len)%row_len) == 0:
+			maze.insert(int(pos[0]), '\n')
+	maze_view = " ".join([str(i) for i in maze])
 	print(maze_view)
-#Effectuer un retour a la ligne tous les (row_leng) caracteres, utiliser une REGEX
-#Positioner des espace entre chaques caracteres, REGEX?
 
-maze_view()
+maze_view(maze)
 
 #While True:
 	#Current position of 2 (MacGyver/Player)
 pos = maze.index(2)
-#print(pos) # Test command
-	#u = up (position +1 row)
-	#d = down (position (1 row)
-	#r = right (position +1)
-	#l = left (position -1)
-	#q = quit the while infinit loop
+print("player position, in cell: ", pos) # Test command
+
+move = input("""What is your next move?
+	please press:
+	u = Up
+	d = Down
+	l = Left
+	r = right
+	q = quit
+	""")
+if move==r:
+	int(pos) += 1 #r = right (position +1)
+elif move==l:
+	int(pos) -= 1 #l = left (position -1)
+elif move==u:
+	int(pos) += row_len #u = up (position +1 row)
+elif move==d:
+	int(pos) -= row_len #d = down (position (1 row)
+elif move==q:
+	break #q = quit the while infinit loop
+	
+	
+	
+	
+	
 	#All other keys = message d'erreur + rappel des keys (make key input standard, Upper case or lower case doesn't matter)
 	#Attention ne pas depasser le 0 (en x ou y), lorsqu'il est sur les bordures du damier/maze, le joueur ne peux pas en sortir.
-	#If, Elif, Else
 	#If 1 or X in new position = NO! Blocking player. No messages, player kept in place, ask for more instructions.
 	#If 0, Then write 2 in new position, and replace old position by 0.
 
