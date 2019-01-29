917a4db81cad296152074a27ff79261271e48441
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-01-29 17:30:45 +0100

    Cleanning up my code, redefine actions in functions. Re-position win/lose rendering to a more logical place. make it pretty.

--- mazerunner.py
+++ mazerunner.py
@@ -7,35 +7,12 @@ import pygame as pg
 from sys import exit
 from random import randrange as randrange
 
-def maze_view(mylist:list, length:int):
-	"""
-	Maze_view intend to make the list more readable visually, and appealing.
-	This function will display a list, 
-	into a formated shape. The list is sliced and displayed according 
-	to a given length.
-		mylist = list we want to visualise.
-		length = length of a row.
-	"""
-	list_view = ""
-	for pos, tile in enumerate(mylist):
-		path = ""
-		if tile == 0 :
-			path = " "
-		elif tile == 1 :
-			path = "#"
-		elif tile == 2:
-			path = "M"
-		elif tile == 3:
-			path = "G"
-		else:
-			path = str(tile)
-		list_view+=path+" "
-		if (pos + length+1)%length == 0:
-			list_view+="\n"
-	print(list_view)
+white = (255,255,255)
+black = (0,0,0)	
+state = "run"
 
 def antidote(maze:list):
-	"""This function position antidote components randomlyin maze. 
+	"""This function position antidote components randomly in maze. 
 	This function needs random module"""
 	for i in range(4,7):
 		while True:
@@ -51,25 +28,25 @@ def move(maze:list, direction:str, row_len:int):
 	and avoiding getting out of the maze's board, or colliding with walls"""
 	pos = maze.index(2)
 	global victory
-	if direction in ("l", "left"):
+	if direction == pg.K_LEFT:
 		if pos%row_len == 0:
 			return
 		elif maze[pos-1] == 1:
 			return
 		maze[pos-1] = 2
-	elif direction in ("r", "right"):
+	elif direction == pg.K_RIGHT:
 		if (pos+1)%row_len == 0:
 			return
 		elif maze[pos+1] == 1:
 			return
 		maze[pos+1] = 2
-	elif direction in ("u", "up"):
+	elif direction == pg.K_UP:
 		if pos-row_len < 0:
 			return
 		elif maze[pos-row_len] == 1:
 			return
 		maze[pos-row_len] = 2
-	elif direction in ("d", "down"):
+	elif direction == pg.K_DOWN:
 		if pos+row_len >= len(maze):
 			return
 		elif maze[pos+row_len] == 1:
@@ -78,15 +55,28 @@ def move(maze:list, direction:str, row_len:int):
 	else:
 		return
 	maze[pos] = 0
+	winlose(maze)
+
+def winlose(maze:list):
+	"""docstring"""
+	#Count remaining components
+	global state
+	count = 0
+	for n in maze:
+		if n in (range(4,7)):
+			count+=1
+	#Check if there is a guardian, and define if win or lose
+	if maze.count(3) != 0:
+		pass
+	else:
+		if count != 0:
+			state = "lose"
+		else:
+			state = "win"
 
 def main():
 	"""Main function for running this script in Pygame"""
 	pg.init()
-
-	#Music background.
-	pg.mixer.music.set_volume(0.5)
-	pg.mixer.music.load("./media/MacGyver_Theme-8-Bit.ogg")
-	pg.mixer.music.play(3)
 
 	maze = []
 	with open("maze_board/maze_1.txt") as f:
@@ -95,11 +85,9 @@ def main():
 	row_len = int(sqrt(len(maze)))
 	antidote(maze)
 
-	screen = pg.display.set_mode(((row_len*30), (row_len*30)))
-	textures = {i: pg.image.load("./media/{}.png".format(i)) for i in range(1,7)}
-	white = (255,255,255)
-	black = (0,0,0)	
-	state = "run"
+	screen = pg.display.set_mode(((row_len*30), (row_len*30))) #load screen
+	textures = {i: pg.image.load("./media/{}.png".format(i)) for i in range(1,7)} #load textures
+	global state
 
 	while True:
 		screen.fill(black) #Clear Screen (fill screen with black)
@@ -108,68 +96,29 @@ def main():
 			x = (pos%row_len)*30
 			y = (pos//row_len)*30
 			if tile != 0:
-				screen.blit(textures[tile], (x, y))
+				screen.blit(textures[tile], (x, y)) #comment
+
+		if state == "lose":
+			text = pg.font.SysFont("Arial", 40).render("You lost. Try Again!", True, white)
+			screen.blit(text, (screen.get_width()/2 - text.get_rect().width/2,
+				screen.get_height()/2 - text.get_rect().height/2))			
+		elif state == "win":
+			text = pg.font.SysFont("Arial", 40).render("WINNER !", True, white)
+			screen.blit(text, (screen.get_width()/2 - text.get_rect().width/2,
+				screen.get_height()/2 - text.get_rect().height/2))
 
 		pg.display.update()
-
+		
+		#Commentaire boucle evennements
 		for event in pg.event.get():
 			if event.type == pg.KEYDOWN:
-				if event.key == pg.K_ESCAPE:
+				if state != "run":
 					exit()
-				elif event.key == pg.K_LEFT:
-					move(maze, "l", row_len)
-				elif event.key == pg.K_RIGHT:
-					move(maze, "r", row_len)
-				elif event.key == pg.K_UP:
-					move(maze, "u", row_len)
-				elif event.key == pg.K_DOWN:
-					move(maze, "d", row_len)
-
-		pos = maze.index(2)
-
-		count = 0
-		for n in maze:
-			if n in (range(4,7)):
-				count+=1
-			pass
-
-		if maze.count(3) != 0:
-			continue
-		else:
-			if count != 0:
-				state = "lose"
-			else:
-				state = "win"
-			break
-	pg.mixer.music.stop()
-
-	if state == "lose":
-		message = "You lost. Try Again!"
-		pg.mixer.Sound("./media/lost.ogg").play()
-	else:
-		message = "WINNER !"
-		pg.mixer.Sound("./media/win.ogg").play()
-
-	while True:
-		for event in pg.event.get():
-			if event.type == pg.KEYDOWN:
-				if event.key == pg.K_ESCAPE:
-					exit()
-		screen.fill(black) #Clear Screen (fill screen with black)
-		text = pg.font.SysFont("Arial", 40).render(message, True, white)
-		screen.blit(text, (screen.get_width()/2 - text.get_rect().width/2,
-			screen.get_height()/2 - text.get_rect().height/2))
-		pg.display.update()
+				else:
+					if event.key == pg.K_ESCAPE:
+						exit()
+					elif event.key in (pg.K_LEFT, pg.K_RIGHT, pg.K_UP, pg.K_DOWN):
+						move(maze, event.key, row_len)
 
 if __name__ == '__main__':
-	main()
-
-
-#Respecter docstrings & PEP 8 (Max 79 caracteres per lines)
-	
-#Reminder:
-#0 = Floor
-#1 = Wall (cannot go there)
-#2 = MacGyver (Player)
-#3 = Guardian
-#Range(4,7) = Antidot Components (3 ojects to find)
+		main()
