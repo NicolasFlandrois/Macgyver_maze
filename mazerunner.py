ffaf85ddf4569f5e25950ee08bc9cbc3e72bd267
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-01-31 15:25:37 +0100

    Created a countdown rendered for antidot components within draw() striped aside all initial and immuable variables. running check = OK

--- mazerunner.py
+++ mazerunner.py
@@ -8,7 +8,7 @@ from sys import exit
 from random import randrange as randrange
 
 def randomized_components(maze:list):
-	"""This function randomly positions antidote components in maze. 
+	"""Randomly assigning positions for antidote's components in maze. 
 	This function needs to import random module"""
 	for i in range(4,7): #For loop applied to components named (4, 5, 6)
 		while True:
@@ -23,7 +23,6 @@ def move(maze:list, direction:str, row_len:int):
 	"""This function will define the player's moves, 
 	and avoiding getting out of the maze's board, or colliding with walls"""
 	pos = maze.index(2) #Catch player's position
-	global victory
 	if direction == pg.K_LEFT and pos%row_len != 0 and maze[pos-1] != 1:
 		maze[pos-1] = 2
 	elif (direction == pg.K_RIGHT and (pos+1)%row_len != 0 
@@ -42,11 +41,12 @@ def move(maze:list, direction:str, row_len:int):
 def winlose(maze:list):
 	"""This function will define a win/lose situation"""
 	#Count remaining components
-	global state
-	count = 0
+	global state, count
+	ct = 0 #temporary count
 	for n in maze:
 		if n in (range(4,7)):
-			count+=1
+			ct+=1
+	count = ct
 	#Check if there is a guardian, and define if win or lose
 	if maze.count(3) != 0:
 		pass
@@ -57,32 +57,43 @@ def winlose(maze:list):
 			state = "win"
 
 def draw(maze:list, row_len:int):
-	"""DOCSTRING HERE - Render graphics"""
-	screen = pg.display.set_mode(((row_len*30), (row_len*30))) #load screen
-	textures = {i: pg.image.load("./media/{}.png".format(i)) for i in range(1,7)} #load textures	
-	global state
-	global count
+	"""Rendering all graphics"""
+	global state, count, remainct0, remainct1, remainct2, remainct3
+	global textwin, textlose
+
+	screen = (pg.display.set_mode(((row_len*30), 
+	(row_len*30)+remainct0.get_rect().height))) #loading screen
+	
+	textures = {i: pg.image.load("./media/{}.png".format(i)) 
+	for i in range(1,7)} #loading textures
+	
 	screen.fill(black) #Clear Screen (fill screen with black)	
-	#Tetermine tiles' position in pygame coordinates.
+	
+	#Determine tiles' positions in pygame coordinates.
 	for pos, tile in enumerate(maze):
 		x = (pos%row_len)*30
 		y = (pos//row_len)*30
 		if tile != 0:
 			screen.blit(textures[tile], (x, y)) 
-	#Display of remaining componets to pick up
-	remaintxt = (pg.font.SysFont("Arial", 40)
-		.render("Remaining components : ", True, white))
-	screen.blit(remaintxt, (0, 0)
+	
+	#Rendering count of remaining components to pick up
+	if count == 1:
+		remaintxt = remainct1
+	elif count == 2:
+		remaintxt = remainct2
+	elif count == 3:
+		remaintxt = remainct3
+	else:
+		remaintxt = remainct0
+	screen.blit(remaintxt, (0, screen.get_height()- remaintxt.get_rect().height))
+	
 	#Rendering win/lose text on screen, according to state status (run, win, lose)
 	if state == "lose":
-		text = (pg.font.SysFont("Arial", 40)
-			.render("You lost. Try Again!", True, white))
-		screen.blit(text, (screen.get_width()/2 - text.get_rect().width/2,
-			screen.get_height()/2 - text.get_rect().height/2))			
+		screen.blit(textlose, (screen.get_width()/2 - textlose.get_rect().width/2, 
+			screen.get_height()/2 - textlose.get_rect().height/2))			
 	elif state == "win":
-		text = pg.font.SysFont("Arial", 40).render("WINNER !", True, white)
-		screen.blit(text, (screen.get_width()/2 - text.get_rect().width/2,
-			screen.get_height()/2 - text.get_rect().height/2))
+		screen.blit(textwin, (screen.get_width()/2 - textwin.get_rect().width/2, 
+			screen.get_height()/2 - textwin.get_rect().height/2))
 
 	pg.display.update()
 
@@ -105,16 +116,31 @@ def main(maze:list, row_len:int):
 					elif event.key in (pg.K_LEFT, pg.K_RIGHT, pg.K_UP, pg.K_DOWN):
 						move(maze, event.key, row_len)
 
+#Immuable/Initial variables
 white = (255,255,255)
 black = (0,0,0)	
 state = "run"
-#Import the maze from a .txt file, and convert it into a usable list.
+count = 3
+#Import the maze from a maze_1.txt file, and convert it into a usable list.
 maze = []
-with open("maze_board/maze_1.txt") as f:
+with open("./maze_board/maze_1.txt") as f:
 	maze = [int(n) for n in f.read().replace(" ", "").replace("\n", "")]
 
 row_len = int(sqrt(len(maze))) #Determine the length of a row from maze.
 randomized_components(maze) #Randmonly positions components
-
+#Text to render further on, Components' countdown , Win, Lose
+pg.font.init()
+remainct0 = (pg.font.Font("./media/arial.ttf", 30)
+	.render("Remaining components : 0", True, white))
+remainct1 = (pg.font.Font("./media/arial.ttf", 30)
+	.render("Remaining components : 1", True, white))
+remainct2 = (pg.font.Font("./media/arial.ttf", 30)
+	.render("Remaining components : 2", True, white))
+remainct3 = (pg.font.Font("./media/arial.ttf", 30)
+	.render("Remaining components : 3", True, white))
+textlose = (pg.font.Font("./media/arial.ttf", 40)
+	.render("You lost. Try Again!", True, white))
+textwin = pg.font.Font("./media/arial.ttf", 40).render("WINNER !", True, white)
+#Game play
 if __name__ == '__main__':
 		main(maze, row_len)
