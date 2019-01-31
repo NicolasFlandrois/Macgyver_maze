a844dffe786186e6420704b5cba7ec19368b67e3
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-01-31 21:14:23 +0100

    Object Oriented Programming Initial

--- maze.py
+++ maze.py
@@ -5,5 +5,6 @@ class Maze():
 		with open(path) as f:
 			self.tiles = [int(n) for n in f.read().replace(" ", "").replace("\n", "")]
 
+
 	def get_row_length(self):
 		return int(sqrt(len(self.tiles))) #Determine the length of a row from maze.
