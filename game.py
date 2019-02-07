6b7e7c2dc00e56d878ae65555ea13ba54ad76d47
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-02-07 17:08:27 +0100

    added comments date name UTF8 & Py version to all code files

--- game.py
+++ game.py
@@ -1,3 +1,8 @@
+# !usr/bin/python3.6
+# UTF8
+# Date: Thu 07 Feb 2019 04:22:15 PM CET
+# Author: Nicolas Flandrois
+
 from maze import Maze
 import pygame as pg
 
--- maze.py
+++ maze.py
@@ -1,3 +1,8 @@
+# !usr/bin/python3.6
+# UTF8
+# Date: Thu 07 Feb 2019 04:22:15 PM CET
+# Author: Nicolas Flandrois
+
 from math import sqrt as sqrt
 from random import randrange as randrange
 
